5d2310f9760aa21b7c7bcf7c5d034773530e7bdf
indraneeldey2000 <116252120+indraneeldey2000@users.noreply.github.com>
2023-03-26 15:57:48 +0530

    Update Pads.js

--- src/components/Pads.js
+++ src/components/Pads.js
@@ -3,11 +3,16 @@ import Pad from "./Pad";
 import {bank1} from "./App"
 
 
-function Pads() {
+function Pads({power, volume}) {
     const keypadCode = Object.keys(bank1);
 
 
     const playSound = e => {
+      const audio = document.getElementById(e.target.innerText);
+      audio.volume = volume;
+      audio.currentTime =0;
+      audio.play();
+      document.getElementById("display").innerText = bank1[e.target.innerText].name;
     }
 
 
@@ -20,10 +25,11 @@ function Pads() {
               id={pad+idx}
               key={pad+idx}
               handleClick={playSound}
-              element={pad} />
+              element={pad} 
+              power={power}/>
           );
         })}
-        <div id='display'> Show the name of current audio here </div>  
+        <div id='display'>Play a sound! </div>  
       </div>
     )
   }
